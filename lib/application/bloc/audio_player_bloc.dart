4eb5ab7a079db686a3997b50b1ebe08e1bf444a3
winaikongpeng <winai1994@windowslive.com>
2023-03-03 15:41:24 +0700

    audio player state

--- lib/application/bloc/audio_player_bloc.dart
+++ lib/application/bloc/audio_player_bloc.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'audio_player_state.dart';
 
 part 'audio_player_event.dart';
-part 'audio_player_state.dart';
 
 class AudioPlayerBloc extends Bloc<AudioPlayerEvent, AudioPlayerState> {
   AudioPlayerBloc() : super(AudioPlayerInitial()) {
--- lib/application/bloc/audio_player_state.dart
+++ lib/application/bloc/audio_player_state.dart
@@ -1,10 +1,45 @@
-part of 'audio_player_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_music_player/domain/models/audio_player_model.dart';
 
 abstract class AudioPlayerState extends Equatable {
   const AudioPlayerState();
-  
-  @override
-  List<Object> get props => [];
 }
 
-class AudioPlayerInitial extends AudioPlayerState {}
+class AudioPlayerInitial extends AudioPlayerState {
+  @override
+  List<Object?> get props => [];
+}
+
+class AudioPlayerReady extends AudioPlayerState {
+  final List<AudioPlayerModel> listAudios;
+  const AudioPlayerReady(this.listAudios);
+
+  @override
+  List<Object> get props => [listAudios];
+}
+
+class AudioPlayerPlaying extends AudioPlayerState {
+  final List<AudioPlayerModel> listAudios;
+  final AudioPlayerModel playingAudio;
+  const AudioPlayerPlaying(this.playingAudio, this.listAudios);
+
+  @override
+  List<Object> get props => [playingAudio, listAudios];
+}
+
+class AudioPlayerPaused extends AudioPlayerState {
+  final List<AudioPlayerModel> listAudios;
+  final AudioPlayerModel pausedAudio;
+  const AudioPlayerPaused(this.pausedAudio, this.listAudios);
+
+  @override
+  List<Object> get props => [pausedAudio];
+}
+
+class AudioPlayerFailure extends AudioPlayerState {
+  final String error;
+  const AudioPlayerFailure(this.error);
+
+  @override
+  List<Object> get props => [error];
+}
