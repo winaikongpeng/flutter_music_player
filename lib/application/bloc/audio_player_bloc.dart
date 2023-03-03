956ab4129cee0b68959d7c6de03fb6cd9d939c3a
winaikongpeng <winai1994@windowslive.com>
2023-03-03 15:47:38 +0700

    audio player event

--- lib/application/bloc/audio_player_bloc.dart
+++ lib/application/bloc/audio_player_bloc.dart
@@ -1,8 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:equatable/equatable.dart';
+import 'audio_player_event.dart';
 import 'audio_player_state.dart';
-
-part 'audio_player_event.dart';
 
 class AudioPlayerBloc extends Bloc<AudioPlayerEvent, AudioPlayerState> {
   AudioPlayerBloc() : super(AudioPlayerInitial()) {
--- lib/application/bloc/audio_player_event.dart
+++ lib/application/bloc/audio_player_event.dart
@@ -1,7 +1,51 @@
-part of 'audio_player_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_music_player/domain/models/audio_player_model.dart';
 
 abstract class AudioPlayerEvent extends Equatable {
   const AudioPlayerEvent();
+}
+
+class InitialAudio extends AudioPlayerEvent {
+  const InitialAudio();
+
+  @override
+  List<Object> get props => [];
+}
+
+class PressedPlayAudio extends AudioPlayerEvent {
+  final AudioPlayerModel audioPlayerModel;
+  const PressedPlayAudio(this.audioPlayerModel);
+
+  @override
+  List<Object> get props => [audioPlayerModel];
+}
+
+class PressedPauseAudio extends AudioPlayerEvent {
+  final AudioPlayerModel audioPlayerModel;
+  const PressedPauseAudio(this.audioPlayerModel);
+
+  @override
+  List<Object> get props => [audioPlayerModel];
+}
+
+class AudioPlayed extends AudioPlayerEvent {
+  final String audioModelMetaId;
+  const AudioPlayed(this.audioModelMetaId);
+
+  @override
+  List<Object> get props => [audioModelMetaId];
+}
+
+class AudioPaused extends AudioPlayerEvent {
+  final String audioModelMetaId;
+  const AudioPaused(this.audioModelMetaId);
+
+  @override
+  List<Object> get props => [audioModelMetaId];
+}
+
+class AudioStopped extends AudioPlayerEvent {
+  const AudioStopped();
 
   @override
   List<Object> get props => [];
