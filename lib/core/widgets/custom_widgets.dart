0672c6dcf35c0aad155f92730e7aa2f68ea48c74
winaikongpeng <winai1994@windowslive.com>
2023-03-03 19:45:39 +0700

    setup App

--- lib/core/widgets/custom_widgets.dart
+++ lib/core/widgets/custom_widgets.dart
@@ -61,3 +61,30 @@ Widget errorText(String msg) => Center(
         ),
       ),
     );
+
+AppBar customAppBar() {
+  return AppBar(
+    backgroundColor: bgDarkColor,
+    centerTitle: true,
+    actions: [
+      IconButton(
+        onPressed: () {},
+        icon: const Icon(
+          Icons.search,
+          color: whiteColor,
+        ),
+      )
+    ],
+    leading: const Icon(
+      Icons.sort_rounded,
+      color: whiteColor,
+    ),
+    title: Text(
+      'Music player',
+      style: customTextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,13 @@
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
-import 'application/presentation/music_screen.dart';
-import 'core/const/colors.dart';
-import 'core/widgets/custom_app_bar.dart';
+import 'core/app.dart';
 
 void main() {
+  AssetsAudioPlayer.setupNotificationsOpenAction((notification) {
+    // ignore: avoid_print
+    print(notification.audioId);
+    return true;
+  });
+
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter music player',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
-      ),
-      home: const MainScreen(),
-    );
-  }
-}
-
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: bgDarkColor,
-      appBar: customAppBar(),
-      body: const MusicScreen(),
-    );
-  }
 }
